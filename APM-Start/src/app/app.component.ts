eb83e279879012dd9ec535f66db858ad8886a416
Jason Phoon <jasonphoon@gmail.com>
2018-05-04 15:56:07 +0800

    Progress: Encapsulating Progress Styles

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  // template: '<pm-products></pm-products>',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = "Product Page";
 }
